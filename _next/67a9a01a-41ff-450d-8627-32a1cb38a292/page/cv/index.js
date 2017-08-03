a05cfb92272ec3128ed970b4f76bd059437edd98
Andrei Constantinescu <andrei@constantinescu.io>
2017-08-03 16:38:27 +0100

    Realease v1.0.5

---
+++ _next/67a9a01a-41ff-450d-8627-32a1cb38a292/page/cv/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/cv', function() {
+            var comp = module.exports=webpackJsonp([5],{342:function(e,t,u){e.exports=u(343)},343:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(6),r=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return r.default.createElement("h2",null,"The future CV Page.")}}},[342]);
+            return { page: comp.default }
+          })
+        
--- _next/dc2937a9-d4ac-40d1-80f5-3d7d8531b43f/page/cv/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/cv', function() {
-            var comp = module.exports=webpackJsonp([5],{342:function(e,t,u){e.exports=u(343)},343:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(6),o=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return o.default.createElement("h2",null,"The CV Page.")}}},[342]);
-            return { page: comp.default }
-          })
-        
